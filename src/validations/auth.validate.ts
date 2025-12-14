eac01178779203ad06b932c2b582bb9604facdea
mugni1 <asepabdulmugni22@gmail.com>
2025-12-14 12:11:29 +0700

    feat: add user validate

--- src/validations/auth.validate.ts
+++ src/validations/auth.validate.ts
@@ -1,16 +1,16 @@
 import z from "zod";
 
 export const registerSchemaValidate = z.object({
-    name: z.string().min(2).max(50),
-    username: z.string().min(4).max(20),
-    email: z.email().min(2).max(225),
-    password: z.string().min(8).max(12)
+    name: z.string("name is required").min(2, "minimum name must have 2 characters").max(50, "maximum name must have 50 characters"),
+    username: z.string("username is required").min(4, "minimum username must have 4 characters").max(20, "maximum username must have 20 characters"),
+    email: z.email("invalid email").min(2, "minimum email must have 2 characters").max(225, "maximum email must have 225 characters"),
+    password: z.string("password is required").min(8, "minimum password must have 8 characters").max(12, "maximum password must have 12 characters")
 })
 export type RegisterPayload = z.infer<typeof registerSchemaValidate>;
 
 export const loginSchemaValidate = z.object({
-    email: z.email().min(2).max(225),
-    password: z.string().min(8).max(12)
+    email: z.email("invalid email").min(2, "minimum email must have 2 characters").max(225, "maximum email must have 225 characters"),
+    password: z.string("password is required").min(8, "minimum password must have 8 characters").max(12, "maximum password must have 12 characters")
 })
 export type LoginPayload = z.infer<typeof loginSchemaValidate>;
 
---
+++ src/validations/user.validate.ts
@@ -0,0 +1,7 @@
+import z from "zod";
+
+export const updateUserProfile = z.object({
+    name: z.string("name is required").min(2, "minimum name must have 2 characters").max(50, "maximum name must have 50 characters"),
+    username: z.string("username is required").min(4, "minimum username must have 4 characters").max(20, "maximum username must have 20 characters"),
+    bio: z.string("bio is required").min(5, "minimum bio must have 5 characters").max(225, "maximum bio must have 225 characters")
+})
